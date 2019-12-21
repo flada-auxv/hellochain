aa07aeb8fe16835f454008a62c9c96f781755952
flada-auxv <aseknock@gmail.com>
2019-12-21 23:45:19 +0900

    tutorial の内容に合わせる

--- hellochain/cmd/hcd/main.go
+++ hellochain/cmd/hcd/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/cosmos/sdk-tutorials/hellochain/starter"
 	"github.com/tendermint/tendermint/libs/cli"
 
 	app "github.com/cosmos/sdk-tutorials/hellochain"
+	"github.com/cosmos/sdk-tutorials/hellochain/starter"
 )
 
 func main() {
+
 	params := starter.NewServerCommandParams(
 		"hcd",
 		"hellochain AppDaemon",
@@ -17,6 +18,7 @@ func main() {
 
 	serverCmd := starter.NewServerCommand(params)
 
+	// prepare and add flags
 	executor := cli.PrepareBaseCmd(serverCmd, "HC", starter.DefaultNodeHome)
 	err := executor.Execute()
 	if err != nil {
