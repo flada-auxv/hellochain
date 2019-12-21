3d0ab037223e7fbb2716f8d0a7f7715abf4a77cc
flada-auxv <aseknock@gmail.com>
2019-12-21 23:28:10 +0900

    01-simple-app の内容に合わせる

--- hellochain/app.go
+++ hellochain/app.go
@@ -11,20 +11,26 @@ import (
 const appName = "hellochain"
 
 var (
-	// ModuleBasics variable
+	// ModuleBasics holds the AppModuleBasic struct of all modules included in the app
 	ModuleBasics = starter.ModuleBasics
 )
 
 type helloChainApp struct {
-	*starter.AppStarter
+	*starter.AppStarter // helloChainApp extends starter.AppStarter
 }
 
-// NewHelloChainApp function
-// NOTE: 01 の時点だと abci.Application が定義されてないとエラーになる
-func NewHelloChainApp(logger log.Logger, db dbm.DB) abci.Applciation {
+// NewHelloChainApp returns a fully constructed SDK application
+func NewHelloChainApp(logger log.Logger, db dbm.DB) abci.Application {
+
+	// construct our starter to extend
 	appStarter := starter.NewAppStarter(appName, logger, db)
 
-	var app = &helloChainApp{appStarter}
+	// compose our app with starter
+	var app = &helloChainApp{
+		appStarter,
+	}
+
+	// do some final configuration...
 	app.InitializeStarter()
 
 	return app
